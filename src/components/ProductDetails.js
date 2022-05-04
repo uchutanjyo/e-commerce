2c381c83cf5552e65e47c32c81d83651e88e65bf
Matt Engerer <matt.engerer@aol.com>
2022-05-03 23:16:26 -0400

    Reverting to when it worked

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -7,12 +7,12 @@ import { getByPlaceholderText } from '@testing-library/react';
 
 const Products = () => {
 const productId = useParams();
-const {filteredProducts, isLoading, searchTerm, product, redirect, setRedirect, cart, setCart, getCart} = useGlobalContext()
+const { products, product, filtered, redirect, setRedirect, cart, setCart, getCart} = useGlobalContext()
     const [singleProductId, setSingleProductId] = useState([])
 
   const navigate = useNavigate();
 
-     // get products for product details page
+    // get products for product details page
         useEffect(()=> {
             axios.get(`http://localhost:8001/products/${productId.productId}`)
             .then(res => {
@@ -51,10 +51,7 @@ const {filteredProducts, isLoading, searchTerm, product, redirect, setRedirect, 
             }
         }, [cart])
 
-
-
-
-const productDetails = filteredProducts.filter((product) => {
+const productDetails = filtered.filter((product) => {
 
        return singleProductId == product.id
     })
--- src/components/ProductsContainer.js
+++ src/components/ProductsContainer.js
@@ -2,47 +2,12 @@
 import React, {useState, useEffect} from 'react';
 import { useGlobalContext } from '../context/Context';
 import Product from './Product'
-import axios from "axios"; 
+
 
 const ProductsContainer = ({children}) => {
-const {isLoading, filteredProducts, setFilteredProducts, searchTerm, loading, setShow, handleClick} = useGlobalContext()
-  const [products, setProducts] = useState([])
-   console.log('yer')
-          const url =
-    'http://localhost:8001/products'
-  
-        const getProducts =async () => {
-       
-        isLoading(true)
-        try {
-        const data = await axios.get(url);
-        const newProducts = data.data
-        setProducts(newProducts)
-        isLoading(false)
+const {products, setProducts, searchTerm, loading,getProducts, filtered, setShow, handleClick} = useGlobalContext()
 
-        }  catch(error) {
-            console.log('ok')
-        console.log(error);
-        } }
 
-           useEffect(()=> {
-           
-            getProducts()
-        }, [])
-
-                
-let filtered  = products.map((item) => { 
-            const {title} = item;
-            return {...item, title: title.toLowerCase()}})
-            .filter((product)=> {
-    return product.title.includes(searchTerm.toLowerCase())  
-})
-    
-   useEffect(()=> {
-           if (filtered) {
-             setFilteredProducts(filtered)
-           }
-        }, [])
 
 if (loading) {
   return <div className='products'>Loading...</div>
