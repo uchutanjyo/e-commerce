cfc1b025b812838a021963827759a7b55a588c22
Michael Engerer <michael.engerer@hotmail.com>
2022-05-05 00:07:33 -0400

    refactor controllers to be be more concise; use async/await in some controllers

--- server/controllers/shop.js
+++ server/controllers/shop.js
@@ -4,124 +4,65 @@ const { redirect } = require('express/lib/response');
 
 const Product = require('../models/product');
 
- 
-exports.getProducts = (req, res, next) => {
+// each of the following are treated equal by the js compiler:
+// findAll().then(data => { return res.json(data) })
+// findAll().then(data => res.json(data))
+// findAll().then(res.json)
+
+exports.getProducts = (req, res, next) => Product.findAll().then(res.json).catch(error => res.status(500).send('Internal server error.'));
+
+exports.getProduct = (req, res, next) => Product.findByPk(req.params.productId).then(res.json).catch(error => res.status(500).send('Internal server error.'));
+
+exports.getCart = (req, res, next) => req.user.getCart().then(getProducts).then(res.json).catch(error => res.status(500).send('Internal server error.'));
+
+// compare postCart with promises vs async/await, so much easier to read!
+exports.postCart = async (req, res, next) => {
   try {
-  Product.findAll().then(products => {
-    console.log('itworked');
-return res.json(products);
- }).catch(err => console.log(err))
-}
-  catch(err) { console.log(err, 'IT DIDNTWORK') } 
-}
-
-
-exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId;
-Product.findByPk(prodId)
-.then(product => {
-return res.json(product);
-}).catch(err => console.log(err))
-};
-
-exports.getCart = (req, res, next) => {
-req.user.getCart()
-.then(cart => {
-    return cart.getProducts()
-    .then(products => {
-     return res.json(products)
-    }).catch(err => console.log(err))
-    })
-  .catch(err => console.log(err))
-};
-
-exports.postCart = (req,res,next) => {
-  try {
-  const prodId = req.body.productId;
-  let fetchedCart;
-  req.user.getCart()
-  .then(cart=> {
-    fetchedCart = cart;
-    return cart.getProducts({where: {id: prodId}})
-  }).then(products=> 
-    {
-
-    let product;
-    if (products.length > 0) {
-      product = products[0]
-    }
-    let newQuantity = 1;
-    if (product) {
-      const oldQuantity = product.cartItem.quantity;
-      newQuantity = oldQuantity + 1;
-
-      return fetchedCart.addProduct(product, {through: {quantity: newQuantity}})
-    }
-    return Product.findByPk(prodId).then(product => {
-                   
-      return res.json(fetchedCart.addProduct(product, {through: {quantity: newQuantity}}))
-
-    })
-  }).catch(err => console.log(err))
-} catch {(err => console.log(err))}
-}
-
-
-// delete item from cart
-exports.postDeleteCartItem = (req, res, next) => {
-    const prodId = req.body.productId.toString()
-  req.user.getCart()
-  .then(cart => {
-    return cart.getProducts({where: {id: prodId}})
-  })
-  .then(products => {
-    console.log(products, 'THE PRODUCTS')
-    const product = products[0];
-    return product.cartItem.destroy();
-  })
-  .then(result => {
-    console.log(prodId, 'prodid')
-return res.json(result)
-  })
-  .catch(err => console.log(err))
+    const { productId } = req.body;
+    const cart = await req.user.getCart();
+    const products = await cart.getProducts({ where: { id: productId }});
+    // use the cart's existing product OR fetch the product
+    const product = products[0] || Product.findByPk(productId);
+    // increment the existing products quantity OR default to 1
+    const quantity = product.cartItem ? product.cartItem.quantity + 1 : 1; 
+    const newProduct = await fetchedCart.addProduct(product, { through: { quantity } });
+    return res.json(newProduct);
+  } catch(error) {
+    return res.status(500).send('Internal server error.');
   }
+};
+
+// carrying on with async/await
+exports.postDeleteCartItem = async (req, res, next) => {
+  try {
+    const { productId } = req.body;
+    const cart = await req.user.getCart();
+    const products = await cart.getProducts();
+    const result = await products[0].cartItem.destroy();
+    return res.json(result);
+  } catch(error) {
+    return res.status(500).send('Internal server error.');
+  }
+};
 
 
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
-req.user.getCart()
-.then(cart => {
-fetchedCart = cart;
-return cart.getProducts();
-}).then(products => {
-req.user.createOrder()
-.then(order => {
-  order.addProducts(products.map((product => {
-    product.orderItem = {quantity: product.cartItem.quantity}
-    return product
-  })))
-})
-.catch(err=>console.log(err))
-})
-.then(result=> {
-
-  fetchedCart.setProducts(null)
-})
-.then(result=> {
-  res.redirect('/orders')
-})
-.catch(err => console.log(err));
-}
-
-exports.getOrders = (req, res, next) => {
-  req.user.getOrders({include: ['products']})
-    .then(orders => {
-  //  shop/orders
-  console.log(orders)
-    }).catch(err => console.log(err))
+  try {
+    const cart = await req.user.getCart();
+    const products = await cart.getProducts();
+    const order = await req.user.createOrder();
+    // spread syntax makes it a little less readable but a lot more concise... trade off lol
+    const orderProducts = products.map(product => ({...product, orderItem: { quantity: product.cartItem.quantity}}));
+    await order.addProducts(orderProducts);
+    await cart.setProducts(null);
+    return res.redirect('/orders');
+  } catch(error) {
+    return res.status(500).send('Internal server error.');
+  }
 };
 
+exports.getOrders = (req, res, next) => req.user.getOrders({ include: ['products'] }).then(res.json).catch('Internal server error.')
 
 exports.getCheckout = (req, res, next) => {
   // res.render('shop/checkout', {
