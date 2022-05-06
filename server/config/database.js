c733f9fb8c326f3604a2d90a339c53f1bbf008bf
Matt Engerer <matt.engerer@aol.com>
2022-05-06 19:09:42 -0400

    testing for netlify/mysql database

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('e-commerce', 'matt', 'mastew', {dialect: 'mysql', host: 'localhost'});
+const sequelize = new Sequelize('sql5490595', 'sql5490595', 'arj7fxmYK8', {dialect: 'mysql', host: '3306'});
 
 module.exports = sequelize;
