830a167ab0599e54f7b2e579a5f6ca4f4c12c7b6
Matt Engerer <matt.engerer@aol.com>
2022-05-07 16:37:51 -0400

    trying to connect to remote mysql server

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('sql5490595', 'sql5490595', 'arj7fxmYK8', {dialect: 'mysql', host: '3306'});
+const sequelize = new Sequelize('sql5490595', 'sql5490595', 'arj7fxmYK8', {dialect: 'mysql', host: 'sql5.freemysqlhosting.net'});
 
 module.exports = sequelize;
