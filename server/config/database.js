1638fa163ff7459f0cbe9e06a95ad789870f5334
Matt Engerer <matt.engerer@aol.com>
2022-05-07 19:04:49 -0400

    trying again - connect to database

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('sql5490595', 'sql5490595', 'arj7fxmYK8', {dialect: 'mysql', host: 'sql5.freemysqlhosting.net'});
+const sequelize = new Sequelize('sql5.freemysqlhosting.net', 'sql5490595', 'arj7fxmYK8', {dialect: 'mysql', host: 'ip-172-31-29-229'});
 
 module.exports = sequelize;
