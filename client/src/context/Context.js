eaa9d7700d3c3890b73363c12ee33af4a8201f46
Matt Engerer <matt.engerer@aol.com>
2022-05-12 15:04:13 -0400

    trying to re-deploy frontend

--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import React, {useState, useCallback, useContext, useEffect} from 'react'
-import reducer from './reducer'
 
 const AppContext = React.createContext();
 
@@ -16,7 +15,6 @@ const AppProvider = ({children}) => {
         const [deleted, setDeleted] = useState(false)
         const [redirect, setRedirect] = useState(false)
  
-
    const cartUrl =
     'https://the-indispensable.herokuapp.com/cart'
 
@@ -39,8 +37,6 @@ const AppProvider = ({children}) => {
          useEffect(()=> {
             getCart()
         }, [deleted]) 
-
-    
 
           const url =
     'https://the-indispensable.herokuapp.com/products'
@@ -68,11 +64,7 @@ let filtered  = products.map((item) => {
             .filter((product)=> {
     return product.title.includes(searchTerm.toLowerCase())  
 })
-
-
-
 // really bad workaround for an issue i was having. in order to make product search case-insensitive, i  mapped products array (state) to a new array, and converted the title property to lowercase.this new array is then filtered - a check is done against the searchTerm (what is typed in the search box) which is also converted to lowercase. the title of each product is then converted to uppercase later on rendering. 
- 
 
     return<>
     <AppContext.Provider value={{
